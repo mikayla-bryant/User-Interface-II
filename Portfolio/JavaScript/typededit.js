cea8d46a2b44d2b25ca6b8b094fb2062a0f153da
mikayla-bryant <mikaylabryant25@gmail.com>
2020-10-29 19:22:03 -0400

    Original Portfolio Draft

---
+++ Portfolio/JavaScript/typededit.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".type", {
+    strings: [
+        'full stack developer.', 
+        'blogger.',
+        'music enthusiast.',
+        'friend you can count on.',
+    ],
+    typeSpeed: 75,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+});
+
